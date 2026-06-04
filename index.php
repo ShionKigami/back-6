a1856b9c877e037bda3cd64bb863c3eb5f307f91
ShionKigami <steblewets.ant@gmail.com>
2026-06-04 23:45:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 
-$user = 'u82197';
-$pass = '6410666';
-$db = new PDO('mysql:host=localhost;dbname=u82197', $user, $pass, [
+$user = 'u82624';
+$pass = '8440989';
+$db = new PDO('mysql:host=localhost;dbname=u82624', $user, $pass, [
     PDO::ATTR_PERSISTENT => true,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
